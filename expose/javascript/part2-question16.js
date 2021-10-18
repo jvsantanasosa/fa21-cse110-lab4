93ce998f28ba28d3e0e2adcb2db4b9ed2cbae41d
jvsantanasosa <74219185+jvsantanasosa@users.noreply.github.com>
2021-10-17 19:27:00 -0700

    Create part2-question16.js

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,15 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2
+};
+
+for(const property in statistics){
+  if((property.charAt(0) === 'r')) || (statistics[properties] % 2 === 1)){
+    console.log(statistics[property]);
+  }
+}
+              
